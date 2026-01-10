2e1adb2bf7457b1a685e30cdbc249d253766d29e
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-01-11 00:46:43 +0200

    Create login route

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,68 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/utils/db";
+import { LoginDTO } from "@/app/utils/dtos";
+import bcrypt from "bcryptjs";
+import { loginSchema } from "@/app/utils/validationSchemas";
+
+/**
+ * @Route POST ~/api/users/login
+ * @Summary Login registered users
+ * @Tags Users
+ * @Accept json
+ * @Produce json
+ * @description this method allows regitered users to login
+ * @Route /api/users/login
+ * @access public
+ */
+
+// create a POST method to allow registered users to login
+export async function POST(request: NextRequest, response: NextResponse) {
+  try {
+    // get user's data from body
+    const body = (await request.json()) as LoginDTO;
+
+    const validation = loginSchema.safeParse(body);
+
+    if (!validation.success) {
+      return NextResponse.json(
+        { message: validation.error.issues[0].message },
+        { status: 400 }
+      );
+    }
+
+    // search for registered users in database
+    const registeredUser = await prisma.user.findUnique({
+      where: { email: body.email },
+    });
+
+    // check if user registered in database or not
+    if (!registeredUser) {
+      return NextResponse.json(
+        { message: "البيانات غير صحيحة" },
+        { status: 400 }
+      );
+    }
+
+    // compare entered password with hashed password in database
+    const password = await bcrypt.compare(
+      body.password,
+      registeredUser.password
+    );
+
+    // check entered password it is valid or not
+    if (!password) {
+      return NextResponse.json(
+        { message: "البيانات غير صحيحة" },
+        { status: 400 }
+      );
+    }
+
+    // return response with success login
+    return NextResponse.json(
+      { message: "تم تسجيل الدخول بنجاح" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
