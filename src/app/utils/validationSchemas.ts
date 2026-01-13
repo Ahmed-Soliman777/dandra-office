fe186e37929f9adbaf29816be9ddec403aee871c
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-01-14 00:24:27 +0200

    Add update user profile schema

--- src/app/utils/validationSchemas.ts
+++ src/app/utils/validationSchemas.ts
@@ -47,3 +47,29 @@ export const loginSchema = z.object({
   email: z.email({ error: "البريد الالكتروني مطلوب" }),
   password: z.string({ error: "كلمة المرور مطلوبة" }),
 });
+
+//validation schema for user login
+export const updateUserProfileSchema = z.object({
+  username: z
+    .string()
+    .min(2, { error: "مطلوب أكثر من حرفين لأنشاء اسم المستخدم" })
+    .max(25, {
+      error: "لقد تعديت الحد المسموح من الحروف لأنشاء اسم المستخدم",
+    })
+    .optional(),
+  /****************************************** */
+  email: z.email().min(2).max(100).optional(),
+  /****************************************** */
+  password: z
+    .string()
+    .regex(
+      passwordRegex,
+      "لأنشاء كلمة السر يجب أن تكون حدود 2 - 25 من الحروف، مع احتواء كلمة السر لحرف Capitail و Small ورقم ورمز مميز"
+    )
+    .min(2, {
+      error:
+        "لأنشاء كلمة السر يجب أن تكون حدود 2 - 25 من الحروف، مع احتواء كلمة السر لحرف Capitail و Small ورقم ورمز مميز",
+    })
+    .max(25, { error: "لقد تعديت الحد المسموح لأنشاء كلمة السر" })
+    .optional(),
+});
