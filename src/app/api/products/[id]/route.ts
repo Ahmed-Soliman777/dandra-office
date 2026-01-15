4f3534c53bd3297b362766648d3281629c08829b
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-01-15 23:13:09 +0200

    Create products route

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,139 @@
+import prisma from "@/app/utils/db";
+import { UpdateProductDTO } from "@/app/utils/dtos";
+import { ProductID } from "@/app/utils/types";
+import { updateProduct } from "@/app/utils/validationSchemas";
+import { verifyToken } from "@/app/utils/verifyToken";
+import { NextRequest, NextResponse } from "next/server";
+
+/**
+ * @route ~/api/products/:id
+ * @method GET
+ * @access public
+ * @description read product
+ */
+
+export async function GET(request: NextRequest, props: ProductID) {
+  try {
+    const { id } = await props.params;
+    const product = await prisma.product.findUnique({
+      where: { id: parseInt(id) },
+      select: {
+        productNameAr: true,
+        productNameEn: true,
+        price: true,
+        quantity: true,
+        category: {
+          select: {
+            categoryNameAr: true,
+            categoryNameEn: true,
+          },
+        },
+        descriptionAr: true,
+        descriptionEn: true,
+        images: true,
+      },
+    });
+    if (!product) {
+      return NextResponse.json({ message: "منتج غير موجود" }, { status: 400 });
+    }
+    return NextResponse.json(product, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
+
+/**
+ * @route ~/api/products/:id
+ * @method Delete
+ * @access private only admin
+ * @description delete product
+ */
+
+export async function DELETE(request: NextRequest, props: ProductID) {
+  try {
+    const { id } = await props.params;
+    const product = await prisma.product.findUnique({
+      where: { id: parseInt(id) },
+    });
+    if (!product) {
+      return NextResponse.json({ message: "منتج غير موجود" }, { status: 400 });
+    }
+    const userPayload = verifyToken(request);
+    if (userPayload === null || userPayload.isAdmin === false) {
+      return NextResponse.json(
+        { message: "غير مصرح بحذف المنتج" },
+        { status: 403 }
+      );
+    }
+    await prisma.product.delete({ where: { id: parseInt(id) } });
+    return NextResponse.json(
+      { message: "تم حذف المنتج بنجاح" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
+
+/**
+ * @route ~/api/products/:id
+ * @method Put
+ * @access private only admin
+ * @description update product
+ */
+
+export async function PUT(request: NextRequest, props: ProductID) {
+  try {
+    const { id } = await props.params;
+    const product = await prisma.product.findUnique({
+      where: { id: parseInt(id) },
+    });
+    if (!product) {
+      return NextResponse.json({ message: "منتج غير موجود" }, { status: 400 });
+    }
+    const userPayload = verifyToken(request);
+    if (userPayload === null || userPayload.isAdmin === false) {
+      return NextResponse.json(
+        { message: "غير مصرح بتعديل المنتج" },
+        { status: 403 }
+      );
+    }
+    const body = (await request.json()) as UpdateProductDTO;
+    const validation = updateProduct.safeParse(body);
+    if (!validation.success) {
+      return NextResponse.json(
+        { message: validation.error.issues[0].message },
+        { status: 400 }
+      );
+    }
+    const updateProducts = await prisma.product.update({
+      where: { id: parseInt(id) },
+      data: {
+        productNameAr: body.productNameAr,
+        productNameEn: body.productNameEn,
+        price: body.price,
+        quantity: body.quantity,
+        categoryId: body.categoryId,
+        descriptionAr: body.descriptionAr,
+        descriptionEn: body.descriptionEn,
+        images: body.images,
+      },
+      select: {
+        productNameAr: true,
+        productNameEn: true,
+        price: true,
+        quantity: true,
+        categoryId: true,
+        descriptionAr: true,
+        descriptionEn: true,
+        images: true,
+      },
+    });
+    return NextResponse.json(
+      { message: "تم تعديل المنتج بنجاح", product: updateProducts },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
