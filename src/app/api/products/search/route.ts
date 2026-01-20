263dac75d2a96d22d7efdacdf90d0ef7f90a73fa
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-01-20 14:49:00 +0200

    Apply filters for the target product

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -1,8 +1,9 @@
 import prisma from "@/app/utils/db";
+import { Prisma } from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 /**
- * @route ~/api/products/search?product=value
+ * @route ~/api/products/search?product=value&minPrice=value&maxPrice=value
  * @method GET
  * @description search for products
  * @access public
@@ -10,39 +11,72 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
+    // get product name from search params
     const search = request.nextUrl.searchParams.get("product");
 
-    let products;
+    // get min price of product from search params
+    const minPrice = request.nextUrl.searchParams.get("minPrice");
 
-    if (search) {
-      products = await prisma.product.findMany({
-        where: {
-          OR: [
-            {
-              productNameAr: {
-                contains: search,
-                mode: "insensitive",
-              },
-            },
-            {
-              productNameEn: {
-                contains: search,
-                mode: "insensitive",
-              },
-            },
-          ],
-        },
-      });
+    // get max price of product from search params
+    const maxPrice = request.nextUrl.searchParams.get("maxPrice");
 
-      if (products.length === 0) {
-        return NextResponse.json(
-          { messsage: "منتج غير متوفر" },
-          { status: 404 },
-        );
-      }
-
-      return NextResponse.json(products, { status: 200 });
+    // user have to apply search so that he could apply filters for the target product
+    if (!search) {
+      return NextResponse.json(
+        { message: "Search term 'product' is required." },
+        { status: 400 },
+      );
     }
+
+    // search for product name weather in arabic or english
+    const whereClause: Prisma.ProductWhereInput = {
+      OR: [
+        {
+          productNameAr: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+        {
+          productNameEn: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+      ],
+    };
+
+    // parse min price to float and apply filters to searched products
+    if (minPrice) {
+      const parsedMinPrice = parseFloat(minPrice);
+      if (!isNaN(parsedMinPrice)) {
+        whereClause.price = {
+          gte: parsedMinPrice,
+        };
+      }
+    }
+
+    // parse max price to float and apply filters to searched products
+    if (maxPrice) {
+      const parsedMaxPrice = parseFloat(maxPrice);
+      if (!isNaN(parsedMaxPrice)) {
+        whereClause.price = {
+          lte: parsedMaxPrice,
+        };
+      }
+    }
+
+    // search for products depending on whereClause variable
+    const products = await prisma.product.findMany({
+      where: whereClause,
+    });
+
+    // if there is no products
+    if (products.length === 0) {
+      return NextResponse.json({ messsage: "منتج غير متوفر" }, { status: 404 });
+    }
+
+    return NextResponse.json(products, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: error });
   }
