9b9b9728114971293cb3addc84f0c637e78fe70a
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-01-21 15:07:30 +0200

    Add favorite route

---
+++ src/app/api/favorites/route.ts
@@ -0,0 +1,93 @@
+import prisma from "@/app/utils/db";
+import { AddFavoriteDTO } from "@/app/utils/dtos";
+import { addFavorite } from "@/app/utils/validationSchemas";
+import { verifyToken } from "@/app/utils/verifyToken";
+import { NextRequest, NextResponse } from "next/server";
+
+/**
+ * @route ~/api/favorites
+ * @method POST
+ * @description add user favorites
+ * @access private
+ */
+
+export async function POST(request: NextRequest) {
+  try {
+    // 1- validate token
+    const userPayload = verifyToken(request);
+    if (userPayload === null) {
+      return NextResponse.json(
+        { message: "سجل دخولك لأضافة المنتج الى المفضلة" },
+        { status: 403 },
+      );
+    }
+
+    // 2- validate favorites
+    const body = (await request.json()) as AddFavoriteDTO;
+    const validation = addFavorite.safeParse(body);
+
+    if (!validation.success) {
+      return NextResponse.json(
+        { message: validation.error.issues[0].message },
+        { status: 400 },
+      );
+    }
+
+    // validate product
+    const product = await prisma.product.findUnique({
+      where: { id: Number(body.productId) },
+    });
+
+    if (!product) {
+      return NextResponse.json({ message: "منتج غير متوفر" }, { status: 400 });
+    }
+
+    // 3- add favorites to db
+    await prisma.favorite.create({
+      data: {
+        userId: userPayload.id,
+        productId: body.productId,
+      },
+    });
+
+    return NextResponse.json(
+      { message: "تم اضافة المنتج الى المفضلة" },
+      { status: 201 },
+    );
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
+
+/**
+ * @route ~/api/favorites
+ * @method GET
+ * @description read user favorites
+ * @access private
+ */
+
+export async function GET(request: NextRequest) {
+  try {
+    // 1- validate token
+    const userPayload = verifyToken(request);
+    if (userPayload === null) {
+      return NextResponse.json(
+        { message: "سجل دخولك لعرض المفضلة" },
+        { status: 403 },
+      );
+    }
+
+    const userFav = await prisma.favorite.findMany({
+      where: { userId: userPayload.id },
+    });
+
+    if (!userFav) {
+      return NextResponse.json({ message: "غير مصرح" }, { status: 403 });
+    }
+
+    // 2- get user favorite
+    return NextResponse.json(userFav, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
