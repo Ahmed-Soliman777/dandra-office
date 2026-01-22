40b6312b17a43b6ba6ad2fe5a7ba6e90b7d81187
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-01-23 01:05:29 +0200

    Creare footer component

---
+++ src/app/components/Footer/FooterWrapper.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { usePathname } from "next/navigation";
+import Footer from "./Footer";
+
+const FooterWrapper = () => {
+    const pathname = usePathname()
+
+    if (pathname.startsWith("/dashboard") || pathname === "/login") return null
+
+    return <Footer />
+}
+
+export default FooterWrapper
