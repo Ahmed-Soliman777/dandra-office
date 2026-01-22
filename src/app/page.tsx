22563427368b99bb7d116ed792fc9eebca3eb06e
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-01-23 01:09:48 +0200

    Add hero component to app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Footer from "./_components/Footer/Footer";
-import Hero from "./_components/Hero/Hero";
-import Navbar from "./_components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 export default function Home() {
   return (
     <div className="bg-background-light dark:bg-background-dark font-display text-[#0c1d1d] dark:text-gray-100 transition-colors duration-300">
-      <Navbar />
       <Hero />
-      <Footer />
     </div>
   );
 }
