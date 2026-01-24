0877827ccc3cb80fa7f207a3cfb1ca83b360fb16
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-01-24 15:02:12 +0200

    Add localization config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,19 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  images:{
+  images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com'
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+
+export default withNextIntl(nextConfig);
