7ccb60952db476292367eb5602f283b1bf83fb4d
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-01-28 01:44:53 +0200

    Add translations props

--- src/app/utils/types.ts
+++ src/app/utils/types.ts
@@ -23,3 +23,20 @@ export interface ReviewID {
 export interface CommentID {
   params: { id: string };
 }
+
+export interface LoginFormProps {
+  loginWelcome: string;
+  emailLabel: string;
+  passwordLabel: string;
+  rememberLabel: string;
+  forgetPassword: string;
+  submitButton: string;
+}
+
+export interface RegisterFormProps {
+  usernameLabel: string;
+  emailLabel: string;
+  passwordLabel: string;
+  confirmPasswordLabel: string;
+  createAccountBtn: string;
+}
