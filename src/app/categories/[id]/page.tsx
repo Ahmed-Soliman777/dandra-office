843e5333ecda45e1c822699b670a04e5f95915ce
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-01-30 01:50:36 +0200

    Create category page

---
+++ src/app/categories/[id]/page.tsx
@@ -0,0 +1,12 @@
+import Category from "./Category"
+
+const page = async (props: { params: { id: string } }) => {
+    const { id } = await props.params
+    return (
+        <div className="min-h-screen">
+            <Category id={id} />
+        </div>
+    )
+}
+
+export default page
