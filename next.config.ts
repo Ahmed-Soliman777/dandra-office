e5660ee61da4befee675446bde1884d060045727
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-02-01 01:43:36 +0200

    Add cloudinary config

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "img.icons8.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
