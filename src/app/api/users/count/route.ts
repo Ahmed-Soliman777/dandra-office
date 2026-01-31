53faff86ba6049265cf819d71f5cf615864a7f0a
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-02-01 01:45:25 +0200

    Create product count api

---
+++ src/app/api/users/count/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import prisma from "@/app/utils/db";
+
+export async function GET() {
+  try {
+    const users = await prisma.user.count({ where: { isAdmin: false } });
+    return NextResponse.json(users, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
