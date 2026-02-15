54bd3ec8aad55919de4986bd7b505efb36b64dc0
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-02-15 02:03:19 +0200

    Add useRouter and remove axios

--- src/app/components/SearchProduct.tsx
+++ src/app/components/SearchProduct.tsx
@@ -1,33 +1,29 @@
 "use client"
-import axios from 'axios'
-import { useState } from 'react'
-import { toast } from 'react-toastify'
-import { DOMAIN } from '../utils/constants'
+import React, { useState } from 'react'
 import { Search } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 const SearchProduct = () => {
     const [searchValue, setSearchValue] = useState<string>('')
-    const [loading, setLoading] = useState<boolean>(false)
 
-    async function handleSearchData() {
-        try {
-            setLoading(false)
-            const { data } = await axios.get(`${DOMAIN}/api/products/search?product=${searchValue}`)
-            console.log(data);
-        } catch (error) {
-            setLoading(false)
-            console.error(error);
-            toast.error("حدث خطأ، حاول مجدداً")
+    const route = useRouter()
+
+    function headToSearch(e: React.FormEvent) {
+        e.preventDefault()
+        if (searchValue.length > 0) {
+            route.push(`/shop/search?product=${searchValue}`)
         }
     }
 
+
     return (
-        <div className="relative group">
+        <form className="relative group" onSubmit={headToSearch}>
             <button
                 className="absolute left-3 top-1/2 -translate-y-1/2 text-primary cursor-pointer"
-                onClick={() => handleSearchData()}
+                // onClick={() => headToSearch()}
+                type='submit'
             >
-                {loading ? <span className="animate-spin size-4 border-2 border-primary border-t-transparent rounded-full block"></span> : <Search size={20} />}
+                <Search size={20} />
             </button>
 
             <input
@@ -37,7 +33,7 @@ const SearchProduct = () => {
                 value={searchValue}
                 onChange={(e) => setSearchValue(e.target.value)}
             />
-        </div>
+        </form>
     )
 }
 
