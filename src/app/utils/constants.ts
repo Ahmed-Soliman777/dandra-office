39820deafe144ea42225460175237cf7acedcf02
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-03-08 22:56:27 +0200

    Fix constants build

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,2 +1,2 @@
 // export const DOMAIN = "http://localhost:3000";
-export const DOMAIN = "https://dandra-office-6razkkklz-ahmed-solimans-projects.vercel.app";
+export const DOMAIN = "https://dandra-office.vercel.app";
