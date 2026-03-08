997ce55342b2442710794d837d51e770db870943
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-03-08 21:25:55 +0200

    update constant file

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,1 +1,2 @@
-export const DOMAIN = "http://localhost:3000";
+// export const DOMAIN = "http://localhost:3000";
+export const DOMAIN = "https://dandra-office-6razkkklz-ahmed-solimans-projects.vercel.app/";
