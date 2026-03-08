ce0304b17c9e6022dc79f3416866dce376ff0c77
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-03-08 23:10:25 +0200

    Add CORS to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,31 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              "https://dandra-office-pytowdvp5-ahmed-solimans-projects.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
